9cc2ca09fc4336b541d013d877acfafb16bb8864
alok0511 <30834567+alok0511@users.noreply.github.com>
2019-01-22 22:22:53 +0530

    Add files via upload

---
+++ Grade.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Grade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int marks = sc.nextInt();
+		if(marks >+ 75)
+			System.out.println("Distinction");
+		else if(marks >= 60 && marks <=74)
+			System.out.println("First Class");
+		else if(marks == 50)
+			System.out.println("Passing");
+		else
+			System.out.println("Fail");
+
+	}
+
+}
