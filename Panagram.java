158a08387b900fa0542ffea215cc385cb5cfeaaa
alok0511 <30834567+alok0511@users.noreply.github.com>
2019-01-22 21:16:10 +0530

    Add files via upload

---
+++ Panagram.java
@@ -0,0 +1,26 @@
+public class Panagram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String s = "The quick brown fox jumps over the lazy dog";
+        int[] a = new int[26];
+        int count =0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)>=65 && s.charAt(i)<=90){
+                if(a[s.charAt(i)-65]==0)
+                    count++;
+                a[s.charAt(i)-65]++;
+            }
+            else if(s.charAt(i)>=97 && s.charAt(i)<=122){
+                if(a[s.charAt(i)-97]==0)
+                    count++;
+                a[s.charAt(i)-97]++;
+            }
+        }
+        if(count==26)
+            System.out.println("pangram");
+        else
+            System.out.println("not pangram");
+    }
+
+}
