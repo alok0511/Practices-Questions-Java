096344e6c1cdb1477d141b882b495ea903fc61d1
alok0511 <30834567+alok0511@users.noreply.github.com>
2019-01-23 16:57:09 +0530

    Update Grade.java

--- Grade.java
+++ Grade.java
@@ -5,11 +5,11 @@ public class Grade {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int marks = sc.nextInt();
-		if(marks >+ 75)
+		if(marks >= 75)
 			System.out.println("Distinction");
 		else if(marks >= 60 && marks <=74)
 			System.out.println("First Class");
-		else if(marks == 50)
+		else if(marks >= 50 && marks <60)
 			System.out.println("Passing");
 		else
 			System.out.println("Fail");
